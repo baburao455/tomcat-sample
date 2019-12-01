852a35785b11ae66633762facf1f9b6d9e2564f1
baburao455 <babub3189@gmail.com>
2019-12-01 13:10:18 -0600

    created new project

---
+++ src/main/java/com/tomcat/samplepp/App.java
@@ -0,0 +1,13 @@
+package com.tomcat.samplepp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
